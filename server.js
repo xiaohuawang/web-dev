2eedae3e53a8f265db57b59c8f01fb47cf7f16db
xiaohua <wxhdax@gmail.com>
2018-02-21 14:19:10 -0800

    Assignment 3

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Get the dependencies
+
 const express = require('express');
 const path = require('path');
 const http = require('http');
@@ -6,15 +7,13 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Point static path to dist -- For building -- REMOVE
 app.use(express.static(path.join(__dirname, 'dist')));
 
-
 // CORS
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
@@ -24,17 +23,15 @@ app.use(function (req, res, next) {
 const port = process.env.PORT || '3100';
 app.set('port', port);
 
-
 // Create HTTP server
 const server = http.createServer(app);
 
 //var serverSide = require("./server/test-mongodb/app");
 //serverSide(app);
 
-
 // For Build: Catch all other routes and return the index file -- BUILDING
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-server.listen(port, () = > console.log('Running on port 3100'));
+server.listen( port , () => console.log('Running'));
